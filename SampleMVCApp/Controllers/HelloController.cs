edd0529b02c2fad826b8d00de193415ead38eb8e
lastar38 <lastarmain@gmail.com>
2021-03-22 22:02:58 +0900

    first Commit.

---
+++ SampleMVCApp/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleMVCApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello! this is sample message!";
+            ViewData["name"] = "";
+            ViewData["mail"] = "";
+            ViewData["tel"] = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form()
+        {
+            ViewData["name"] = Request.Form["name"];
+            ViewData["mail"] = Request.Form["mail"];
+            ViewData["tel"] = Request.Form["tel"];
+            ViewData["Message"] = ViewData["name"] + ", " + ViewData["mail"] + ", " + ViewData["tel"];
+            return View("Index");
+        }
+    }
+}
