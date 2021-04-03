a7f9bce2e808b12898d4e195b0c748931bdadd55
lastar38 <lastarmain@gmail.com>
2021-04-03 22:47:08 +0900

    SQLite

--- SampleMVCApp/Startup.cs
+++ SampleMVCApp/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleMVCApp.Data;
 
 namespace SampleMVCApp
 {
@@ -25,6 +27,9 @@ namespace SampleMVCApp
         {
             services.AddSession();
             services.AddControllersWithViews();
+
+            services.AddDbContext<SampleMVCAppContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("SampleMVCAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
